f2aaf64fbda620def27916c6a3b982ef391c5ee3
Hanson Wong <hwong1994@yahoo.com.au>
2010-10-07 21:09:05 +1100

    Add skeleton front_controller_pre_view listener.

--- upload/library/XenMoods/Listener/FrontControllerPreView.php
+++ upload/library/XenMoods/Listener/FrontControllerPreView.php
@@ -13,63 +13,25 @@ class XenMoods_Listener_FrontControllerPreView
 	 * @param XenForo_FrontController
 	 * @param XenForo_ControllerResponse_Abstract
 	 * @param XenForo_ViewRenderer_Abstract
-	 * @param array 
+	 * @param array Parameters used to help prepare the container
 	 * @return void
 	 */
 	public static function init(XenForo_FrontController $fc, XenForo_ControllerResponse_Abstract &$controllerResponse, XenForo_ViewRenderer_Abstract &$viewRenderer, array &$containerParams)
 	{
-		new self($dependencies, $data);
+		new self($fc, $controllerResponse, $viewRenderer, $containerParams);
 	}
 
 	/**
 	 * Construct and execute code event.
 	 *
-	 * @param XenForo_Dependencies_Abstract Current dependencies object
-	 * @param array Already pre-loaded data
+	 * @param XenForo_FrontController
+	 * @param XenForo_ControllerResponse_Abstract
+	 * @param XenForo_ViewRenderer_Abstract
+	 * @param array Parameters used to help prepare the container
 	 * @return void
 	 */
-	protected function __construct(XenForo_Dependencies_Abstract $dependencies, array $data)
+	protected function __construct(XenForo_FrontController $fc, XenForo_ControllerResponse_Abstract &$controllerResponse, XenForo_ViewRenderer_Abstract &$viewRenderer, array &$containerParams)
 	{
-		// only execute if we are a public-facing view
-		if ($dependencies instanceof XenForo_Dependencies_Public)
-		{
-			$moods = $this->_loadMoodDataRegistry();
-
-			if (!is_array($moods))
-			{
-				$moods = $this->_rebuildMoodCache();
-			}
-			XenForo_Application::set('moods', $moods);
-		}
-	}
-
-	/**
-	 * Load moods from XenForo data registry.
-	 *
-	 * @return array|null List of moods or null if no such entry exists
-	 */
-	protected function _loadMoodDataRegistry()
-	{
-		return XenForo_Model::create('XenForo_Model_DataRegistry')->get('moods');
-	}
-
-	/**
-	 * Helper method to get the mood model.
-	 *
-	 * @return XenMoods_Model_Mood
-	 */
-	protected function _getMoodModel()
-	{
-		return XenForo_Model::create('XenMoods_Model_Mood');
-	}
-
-	/**
-	 * Rebuilds mood cache and inserts it into the database.
-	 *
-	 * @return array Mood cache data
-	 */
-	protected function _rebuildMoodCache()
-	{
-		return $this->_getMoodModel()->rebuildMoodCache();
+		
 	}
 }
