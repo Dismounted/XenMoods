5584b1f6ab54d22a22e31292c3f55a5cfd23ba55
Hanson Wong <hwong1994@yahoo.com.au>
2010-10-10 17:12:21 +1100

    Add code to insert moods on install.

--- upload/library/XenMoods/Install/Data/MySql.php
+++ upload/library/XenMoods/Install/Data/MySql.php
@@ -8,6 +8,14 @@
 class XenMoods_Install_Data_MySql
 {
 	/**
+	 * The location of default mood images in the system. Include forward slash
+	 * before and after path.
+	 *
+	 * @var string
+	 */
+	protected static $moodImageUrlBase = '/styles/default/xenmoods/';
+
+	/**
 	 * Fetches the appropriate queries.
 	 *
 	 * @param integer Version ID of queries to fetch
@@ -53,6 +61,71 @@ $queries[] = "
 
 
 
+$queries[] = "
+	INSERT INTO xf_mood
+		(title, image_url)
+	VALUES
+" . self::_getMoodImageSql();
+
+$queries[] = "
+	UPDATE xf_mood
+	SET is_default = 1
+	WHERE mood_id = 1
+";
+
 		return $queries;
+	}
+
+	/**
+	 * Creates the SQL insert values for mood images.
+	 *
+	 * @return string List of queries to run
+	 */
+	protected static function _getMoodImageSql()
+	{
+		$insertSql = '';
+		$moodImages = self::_getMoodImages();
+		$moodImageUrlBase = self::$moodImageUrlBase;
+
+		foreach ($moodImages AS $name => $path)
+		{
+			$insertSql .= "('{$name}', '{$moodImageUrlBase}{$path}'),\n";
+		}
+
+		return $insertSql;
+	}
+
+	/**
+	 * Fetches all the mood images uploaded into the default directory.
+	 *
+	 * @return array List of mood images uploaded
+	 */
+	protected static function _getMoodImages()
+	{
+		$moodImages = array();
+
+		$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(self::_getRootDir() . self::$moodImageUrlBase));
+		while ($it->valid())
+		{
+			if (!$it->isDot())
+			{
+				$moodName = pathinfo($it->key(), PATHINFO_FILENAME);
+				$moodImages[$moodName] = $it->getSubPathName();
+			}
+
+			$it->next();
+		}
+
+		return $moodImages;
+	}
+
+	/**
+	 * Fetches the XenForo root directory.
+	 *
+	 * @return array List of mood images uploaded
+	 */
+	protected static function _getRootDir()
+	{
+		return XenForo_Application::getRootDir();
 	}
 }
